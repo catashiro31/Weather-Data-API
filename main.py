c1e503bc2a6bf1f01287e57ab8c60dbd8d5e2bfd
catashiro31 <catashiro31@gmail.com>
2025-07-19 16:36:49 +0700

    Commit: Show table and add data

--- main.py
+++ main.py
@@ -1,14 +1,22 @@
+import pandas as pd
 from flask import Flask, render_template
 
 app = Flask(__name__)
 @app.route('/')
 def home():
-    return render_template('home.html')
+    df = pd.read_csv('data/stations.txt', skiprows=17)
+    df = df[['STAID','STANAME                                 ']]
+    return render_template('home.html', data=df.to_html())
 
 @app.route('/api/v1/<station>/<date>')
-def about(station, date):
-    return {"date" : date,
-            "station" : station,
-            "temperature" : 23}
+def query(station, date):
+    filepath = 'data/TG_STAID' + str(station).zfill(6) + '.txt'
+    df = pd.read_csv(filepath, skiprows=20, parse_dates=['    DATE'])
+    temperature = df[df['    DATE'] == date]['   TG'].squeeze() / 10
+    result_dict = {'date' : date,
+                   'station' : station,
+                   'temperature' : temperature}
+    return result_dict
+
 if __name__ == '__main__':
     app.run(debug=True)
