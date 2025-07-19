97a1e3310fd287ff2f3e24a68823a557eaa21e0b
catashiro31 <catashiro31@gmail.com>
2025-07-19 17:25:55 +0700

    Commit: Add URL endpoint and for yearly and all data

--- main.py
+++ main.py
@@ -9,14 +9,38 @@ def home():
     return render_template('home.html', data=df.to_html())
 
 @app.route('/api/v1/<station>/<date>')
-def query(station, date):
-    filepath = 'data/TG_STAID' + str(station).zfill(6) + '.txt'
-    df = pd.read_csv(filepath, skiprows=20, parse_dates=['    DATE'])
-    temperature = df[df['    DATE'] == date]['   TG'].squeeze() / 10
-    result_dict = {'date' : date,
-                   'station' : station,
-                   'temperature' : temperature}
-    return result_dict
+def query_station_date(station, date):
+    try:
+        filepath = 'data/TG_STAID' + str(station).zfill(6) + '.txt'
+        df = pd.read_csv(filepath, skiprows=20, parse_dates=['    DATE'])
+        temperature = df[df['    DATE'] == date]['   TG'].squeeze() / 10
+        result_dict = {'date' : date,
+                       'station' : station,
+                       'temperature' : temperature}
+        return result_dict
+    except FileNotFoundError:
+        return render_template("404_not_found.html")
+
+@app.route('/api/v1/<station>')
+def query_station(station):
+    try:
+        filepath = 'data/TG_STAID' + str(station).zfill(6) + '.txt'
+        df = pd.read_csv(filepath, skiprows=20, parse_dates=['    DATE'])
+        return render_template('station_data.html',
+                               data=df.to_html())
+    except FileNotFoundError:
+        return render_template("404_not_found.html")
+
+@app.route('/api/v1/yearly/<station>/<year>')
+def query_station_year(station, year):
+    try:
+        filepath = 'data/TG_STAID' + str(station).zfill(6) + '.txt'
+        df = pd.read_csv(filepath, skiprows=20, parse_dates=['    DATE'])
+        df = df[df['    DATE'].apply(lambda x : x.year) == int(year)]
+        result = df.to_dict(orient="records")
+        return result
+    except FileNotFoundError:
+        return render_template("404_not_found.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
